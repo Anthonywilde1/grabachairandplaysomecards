aa1ea1fa6d474be4b989ef50e9de2a9df8003128
LachlynR <lachyreynolds@outlook.com>
2020-09-22 12:34:34 +1000

    Added header nav

--- src/gofish/Deck.js
+++ src/gofish/Deck.js
@@ -5,33 +5,51 @@ class Deck {
     )
       .then((response) => response.json())
       .then((data) => data.deck_id);
+
   }
 
+
+
   async draw(n) {
-    let url = `https://deckofcardsapi.com/api/deck/${await this
-      .id}/draw/?count=${n}`;
+    let url = `https://deckofcardsapi.com/api/deck/${await this.id}/draw/?count=${n}`;
     let cardCodes = await fetch(url)
       .then((response) => response.json())
       .then((data) => data.cards.map((card) => card.code));
+    
+      
     return cardCodes;
   }
 
   async newPile(n, name) {
-    let codes = await this.draw(n)
-    let url = `https://deckofcardsapi.com/api/deck/${await this
-      .id}/pile/${name}/add/?cards=${codes.join(",")}`;
+    let codes = await this.draw(n);
+    let url = `https://deckofcardsapi.com/api/deck/${await this.id}/pile/${name}/add/?cards=${codes.join(",")}`;
     let pile = await fetch(url)
       .then((response) => response.json())
       .then((data) => data);
-    return pile
+
+    return pile;
   }
 
   async listPile(name) {
-    let url = `https://deckofcardsapi.com/api/deck/${await this
-      .id}/pile/${name}/list/`;
+    let url = `https://deckofcardsapi.com/api/deck/${await this.id}/pile/${name}/list/`;
     let pileList = await fetch(url)
       .then((response) => response.json())
       .then((data) => data);
+
     return pileList.piles;
+  }
+
+  async pickupCard(name) {
+    let drawnCardCode = await this.draw(1);
+    console.log(drawnCardCode)
+    let url = `https://deckofcardsapi.com/api/deck/${await this.id}/pile/${name}/add/?cards=${drawnCardCode.join("")}`
+    let pile = fetch(url).then(response =>  response.json()).then(data => data)
+    return pile;
+  }
+
+  async currentPileSize(name) {
+    let url = `https://deckofcardsapi.com/api/deck/${await this.id}/pile/${name}/list/`
+    let pile = fetch(url).then(response =>  response.json()).then(data => data)
+    console.log(pile)
   }
 }
